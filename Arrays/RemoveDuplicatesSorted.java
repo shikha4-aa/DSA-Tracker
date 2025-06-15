e2eb8fec5c18fb5b7f2244baf5234114b178b6c0
shikha4-aa <shikhamandal2005@gmail.com>
2025-06-15 11:54:05 +0530

    added some core observations

--- Arrays/RemoveDuplicatesSorted.java
+++ Arrays/RemoveDuplicatesSorted.java
@@ -1,3 +1,12 @@
+//Problem: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+/*Core Observations:
+1. Input Array is Sorted:
+  That’s why duplicates are consecutive. We don’t need a hashmap or set.
+2. In-place Modification:
+We are not allowed to use extra space.
+So two-pointer technique is a must.  */
+
 class Solution {
     public int removeDuplicates(int[] nums) {
         int i= 0;
