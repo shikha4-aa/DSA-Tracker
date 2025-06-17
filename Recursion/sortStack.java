8980c72bf37b597072a0468fe837dbc96c7438f9
shikha4-aa <shikhamandal2005@gmail.com>
2025-06-17 11:31:28 +0530

    solved it using recursion

--- Recursion/sortStack.java
+++ Recursion/sortStack.java
@@ -1,22 +1,45 @@
-import java.util.ArrayList;
-import java.util.Collections;
+//Link: https://practice.geeksforgeeks.org/problems/sort-a-stack/1
 import java.util.Stack;
+        //normal stack sort
+        // ArrayList<Integer> arr = new ArrayList<>();
+        
+        // while(!s.isEmpty()){
+        //     arr.add(s.pop());
+        // }
+        
+        // Collections.sort(arr);
+        
+        
+        // for(int num: arr){
+        //     s.push(num);
+        // }
+        // return s;
+        
+        //using recursion
+        /*Complete the function below*/
 class GfG {
     public Stack<Integer> sort(Stack<Integer> s) {
-        //normal stack sort
-        ArrayList<Integer> arr = new ArrayList<>();
+        // add code here.
+       if(!s.isEmpty()){
+           int temp = s.pop();
+           sort(s);
+           insert(s, temp);
+       }
+       
+       
+    return s;
         
-        while(!s.isEmpty()){
-            arr.add(s.pop());
+    }
+    
+    private void insert(Stack<Integer> s, int temp){
+        if(s.isEmpty() || s.peek()<= temp){
+            s.push(temp);
+            return;
         }
-        
-        Collections.sort(arr);
-        
-        
-        for(int num: arr){
-            s.push(num);
-        }
-        return s;
+        int top = s.pop();
+        insert(s, temp);
+        s.push(top);
         
     }
 }
+    
