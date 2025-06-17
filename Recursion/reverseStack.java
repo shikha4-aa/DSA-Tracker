5122397b93f23c802c009d5af83afb8a3d9571bd
shikha4-aa <shikhamandal2005@gmail.com>
2025-06-17 12:37:26 +0530

    solved it using recursion

---
+++ Recursion/reverseStack.java
@@ -0,0 +1,26 @@
+// User function Template for Java
+import java.util.Stack;
+class Solution {
+    static void reverse(Stack<Integer> s) {
+        // add your code herek
+        if(!s.isEmpty()){
+            int temp = s.pop();
+            reverse(s);
+            insert(s, temp);
+            
+        }
+       return;
+    }
+    
+    public static void insert(Stack<Integer> s, int temp){
+        
+        if(s.isEmpty()){
+            s.push(temp);
+            return;
+        }
+        
+        int top = s.pop();
+        insert(s, temp);
+        s.push(top);
+    }
+}
