57d3e3e519668468d581b80cee44e740f91bbc62
shikha4-aa <shikhamandal2005@gmail.com>
2025-06-25 22:31:35 +0530

    solved

---
+++ SlidingWindow/search.java
@@ -0,0 +1,39 @@
+//Link: https://practice.geeksforgeeks.org/problems/count-the-anagrams-of-a-string/1
+//Problem: Count the Anagrams of a String
+import java.util.Arrays;
+
+class Solution {
+
+    int search(String pat, String txt) {
+        // code here
+        int k = pat.length();
+        int n = txt.length();
+        int count = 0;
+
+        // Frequency array for pattern
+        int[] patFreq = new int[26];
+        for (char c : pat.toCharArray()) {
+            patFreq[c - 'a']++;
+        }
+
+        // Frequency array for current window in text
+        int[] windowFreq = new int[26];
+        for (int i = 0; i < n; i++) {
+            // Add current character to window
+            windowFreq[txt.charAt(i) - 'a']++;
+
+            // Remove character left out of window
+            if (i >= k) {
+                windowFreq[txt.charAt(i - k) - 'a']--;
+            }
+
+            // If window size is k, compare
+            if (i >= k - 1 && Arrays.equals(patFreq, windowFreq)) {
+                count++;
+            }
+        }
+
+        return count;
+        
+    }
+}
