e0c39116de07b9665b2f17e78fc67056bc09ce29
shikha4-aa <shikhamandal2005@gmail.com>
2025-07-01 14:57:33 +0530

    modified

--- Recursion/subsets.java
+++ Recursion/subsets.java
@@ -1,25 +1,24 @@
-//Problem: https://leetcode.com/problems/subsets/
-import java.util.*;
-
+//Link: https://leetcode.com/problems/subsets/
+import java.util.ArrayList;
+import java.util.List;
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         backtrack(0, nums, new ArrayList<>(), result);
+
         return result;
-        
-
     }
 
-   private void backtrack(int index, int[] nums, List<Integer> current, List<List<Integer>> result){
-    result.add(new ArrayList<>(current));
-
-    for(int i = index; i<nums.length; i++){
-        current.add(nums[i]);
-
-        backtrack(i+1, nums, current, result);
-
-        current.remove(current.size()-1);
+    public void backtrack(int index, int[] nums, ArrayList<Integer> current, List<List<Integer>> result){
+        if(index == nums.length){
+        result.add(new ArrayList(current));
+        return;
     }
 
-}
+        current.add(nums[index]);
+        backtrack(index+ 1, nums, current, result);
+
+        current.remove(current.size()- 1); // backtrack (exclude the current element)
+         backtrack(index+ 1, nums, current, result);
+    }
 }
