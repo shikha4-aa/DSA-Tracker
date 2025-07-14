c3b18b22019f24f283de9a032a47b3bbae25bc0a
shikha4-aa <shikhamandal2005@gmail.com>
2025-07-14 21:09:11 +0530

    used an optimal approach

--- Arrays/rearrangeArray.java
+++ Arrays/rearrangeArray.java
@@ -3,7 +3,7 @@
 
 import java.util.ArrayList;
 import java.util.List;
-class Solution {
+class BruteForceSolution {
     public int[] rearrangeArray(int[] nums) {
         List<Integer> pos = new ArrayList<>();
         List<Integer> neg = new ArrayList<>();
@@ -23,6 +23,25 @@ class Solution {
         while(p<pos.size() && n<neg.size()){
             result[i++] = pos.get(p++);
             result[i++] = neg.get(n++);
+        }
+        return result;
+    }
+}
+
+class optimalSolution {
+    public int[] rearrangeArray(int[] nums) {
+
+        int[] result = new int[nums.length];
+        int p = 0, n= 1;
+
+        for(int num: nums){
+            if(num>0){
+                result[p] = num;
+                p+=2;
+            } else{
+                result[n] = num;
+                n+=2;
+            }
         }
         return result;
     }
