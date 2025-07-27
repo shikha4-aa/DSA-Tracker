e7fdb9ea3429cad8b5a06d83caf27e4c62be1393
shikha4-aa <shikhamandal2005@gmail.com>
2025-07-27 21:35:35 +0530

    solved

--- Arrays/MoveZeros.java
+++ Arrays/MoveZeros.java
@@ -14,3 +14,11 @@ class Solution {
         }
     }
 }
+//Link: https://leetcode.com/problems/move-zeroes/
+//Problem: Given an integer array nums, move all 0's to the end of it
+// while maintaining the relative order of the non-zero elements.
+// Example: Input: [0,1,0,3,12] Output: [1,3,12,0,0]
+// Explanation: The non-zero elements are 1, 3, and 12, which are moved to the front of the array,
+// while the zeros are moved to the end, maintaining their relative order.
+// Constraints: 1 <= nums.length <= 10^4
+// 0 <= nums[i] <= 100
