3ddcd33e3f109923d7dd0eabeb85cd78a609c50f
shikha4-aa <shikhamandal2005@gmail.com>
2025-07-27 23:24:24 +0530

    solved using brute force

---
+++ LinkedList/getIntersectionNode.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class getIntersectionNode {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+         if(headA== null || headB == null) return null;
+       while(headA!= null){
+         ListNode tempB= headB;
+
+           while(tempB!= null){
+            if(headA== tempB) return headA;
+
+            tempB= tempB.next;
+           }
+
+           headA= headA.next;
+       }
+   return null;
+    }
+}
+
+// Link: https://leetcode.com/problems/intersection-of-two-linked-lists/
