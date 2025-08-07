54e87bfbc930ccd7fb479e390b16c8c3673f7286
shikha4-aa <shikhamandal2005@gmail.com>
2025-08-07 23:23:18 +0530

    solved

---
+++ Stacks/nextGreater.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] res = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            int index = -1;
+
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    index = j;
+                    break;
+                }
+            }
+
+            int nextGreater = -1;
+            for (int k = index + 1; k < nums2.length; k++) {
+                if (nums2[k] > nums1[i]) {
+                    nextGreater = nums2[k];
+                    break;
+                }
+            }
+
+            res[i] = nextGreater;
+        }
+
+        return res;
+    }
+}
+
+// Problem link: https://leetcode.com/problems/next-greater-element-i/
