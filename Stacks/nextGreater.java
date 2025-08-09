78b9698defcc1572eae985b1c65f115e3ce74528
shikha4-aa <shikhamandal2005@gmail.com>
2025-08-09 23:45:54 +0530

    solved using stack

--- Stacks/nextGreater.java
+++ Stacks/nextGreater.java
@@ -1,30 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] res = new int[nums1.length];
+      Map<Integer, Integer> greater = new HashMap<>();
+      Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i < nums1.length; i++) {
-            int index = -1;
+      for(int i = nums2.length-1; i>=0; i--){
 
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i] == nums2[j]) {
-                    index = j;
-                    break;
-                }
-            }
-
-            int nextGreater = -1;
-            for (int k = index + 1; k < nums2.length; k++) {
-                if (nums2[k] > nums1[i]) {
-                    nextGreater = nums2[k];
-                    break;
-                }
-            }
-
-            res[i] = nextGreater;
+        while(!stack.isEmpty() && stack.peek()<= nums2[i]){
+            stack.pop();
         }
 
-        return res;
+
+        if(stack.isEmpty()){
+            greater.put(nums2[i], -1);
+        } else{
+            greater.put(nums2[i], stack.peek());
+        }
+
+        stack.push(nums2[i]);
+      }
+      
+      int[] res= new int[nums1.length];
+      for(int i = 0; i<nums1.length; i++){
+        res[i] = greater.get(nums1[i]);
+
+      }
+  return res;
     }
 }
 
-// Problem link: https://leetcode.com/problems/next-greater-element-i/
+//link: https://leetcode.com/problems/next-greater-element-i/
