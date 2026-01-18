2d3c9384efdc3dad7952840b412aa746c01b5661
Shikha <shikhamandal2005@gmail.com>
2026-01-18 19:09:38 +0530

    solved

---
+++ Arrays/Sorting/bubbleSortRecursive.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+// brute force approach
+public class bubbleSortRecursive {
+
+    public static void bubbleSort(int[] arr, int n) {
+        // base case
+        if (n <= 1) return;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+
+        bubbleSort(arr, n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        bubbleSort(arr, n);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i]);
+            if (i < n - 1) sb.append(" ");
+        }
+        System.out.println(sb.toString());
+        sc.close();
+    }
+}
