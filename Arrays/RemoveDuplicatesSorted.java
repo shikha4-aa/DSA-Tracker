531d5950f9a1ac58cb6699907c85323006bbec68
Shikha <shikhamandal2005@gmail.com>
2026-02-03 21:12:31 +0530

    solved

--- Arrays/RemoveDuplicatesSorted.java
+++ Arrays/RemoveDuplicatesSorted.java
@@ -7,8 +7,8 @@
 We are not allowed to use extra space.
 So two-pointer technique is a must.  */
 
-class Solution {
-    public int removeDuplicates(int[] nums) {
+class removeDuplicates {
+    public int removeDuplicatesArray(int[] nums) {
         int i= 0;
         for(int j=1; j<nums.length; j++){
             if(nums[i]!= nums[j]){
@@ -17,6 +17,29 @@ class Solution {
             }
         }
         return i+1;
+    }
 
-}
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        
+        System.out.print("Enter the size of array: ");
+        int n = scanner.nextInt();
+        
+        int[] nums = new int[n];
+        System.out.println("Enter " + n + " sorted numbers:");
+        for(int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        
+        removeDuplicates obj = new removeDuplicates();
+        int count = obj.removeDuplicatesArray(nums);
+        
+        System.out.println("\nArray after removing duplicates:");
+        for(int i = 0; i < count; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println("\n\nNumber of unique elements: " + count);
+        
+        scanner.close();
+    }
 }
